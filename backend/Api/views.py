77836d4e44d1f76de146b6d34aa40aa471403ced
Stephon Brown <stephonb14@gmail.com>
2020-07-06 01:41:30 -0400

    added react application and updated urls

--- backend/Api/views.py
+++ backend/Api/views.py
@@ -1,3 +1,10 @@
+from .models import ToDo
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ToDoSerializer
 
 # Create your views here.
+class ToDoView(viewsets.ModelViewSet):
+    serializer_class = ToDoSerializer
+    queryset = ToDo.objects.all()
+
